492626cd2dfcaa23f161f33a069facb34d74669a
HrEsben <esben@optus.dk>
2024-05-24 00:53:43 +0200

    Ensure locations.geojson is copied to dist and read from there3

--- fetch-geojson.js
+++ fetch-geojson.js
@@ -6,6 +6,12 @@ const url = "https://optus.dk/locations.geojson";
 const targetPath = path.resolve(__dirname, "src/locations.geojson");
 const distPath = path.resolve(__dirname, "dist/locations.geojson");
 
+// Ensure the dist directory exists
+const distDir = path.resolve(__dirname, "dist");
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
 https
   .get(url, (response) => {
     const file = fs.createWriteStream(targetPath);
