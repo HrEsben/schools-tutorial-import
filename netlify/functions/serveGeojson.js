ff0ac3163031b832878953a84b68f5c080deb1a5
HrEsben <esben@optus.dk>
2024-05-23 14:07:42 +0200

    Fix Netlify function to serve geojson correctly

--- netlify/functions/serveGeojson.js
+++ netlify/functions/serveGeojson.js
@@ -2,13 +2,24 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async function (event, context) {
-  const filePath = path.resolve(__dirname, "../../locations.geojson");
-  const fileContent = fs.readFileSync(filePath, "utf8");
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: fileContent,
-  };
+  try {
+    const filePath = path.resolve(__dirname, "../../locations.geojson");
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: fileContent,
+    };
+  } catch (error) {
+    console.error("Error reading locations.geojson:", error);
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ error: "Failed to read locations.geojson" }),
+    };
+  }
 };
